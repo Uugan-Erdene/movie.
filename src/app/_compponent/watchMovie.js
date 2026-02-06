53e1b7bd7d723ebc4dae5bc522abae38a7425b76
25LP5949 25LP5949 <25LP5949@G305-CC0244.local>
2026-02-06 12:39:43 +0800

    redmi

--- src/app/_compponent/watchMovie.js
+++ src/app/_compponent/watchMovie.js
@@ -37,7 +37,6 @@ export const WatchMovie = () => {
     <div className="w-full h-150 overflow-x-auto overflow-y-hidden flex flex-row snap-x snap-mandatory">
       {movies.map((movie) => (
         <div key={movie.id} className="relative min-w-full h-full snap-center ">
-          {/* Image */}
           <img
             src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
             alt={movie.title}
@@ -46,44 +45,35 @@ export const WatchMovie = () => {
 
           {/* Overlay content */}
           <div className="absolute top-0 left-0 w-full h-full flex justify-between items-center pl-36 gap-6 bg-black/30">
-            <div className="pl-12">
-              <div className="w-10 h-10 bg-white cursor-pointer flex justify-center items-center rounded-full">
-                <button className="cursor-pointer">
-                  <LeftBullet />
-                </button>
-              </div>
-              {/* Left content */}
-              <div className="flex flex-col gap-6 max-w-175">
-                <div>
-                  <h2 className="text-white text-lg">{movie.original_title}</h2>
+            <div className="flex flex-col gap-6 max-w-175">
+              <div>
+                <h2 className="text-white text-lg">{movie.original_title}</h2>
 
-                  <div className="flex flex-col gap-2">
-                    <h1 className="text-white text-4xl font-bold">
-                      {movie.title}
-                    </h1>
+                <div className="flex flex-col gap-2">
+                  <h1 className="text-white text-4xl font-bold">
+                    {movie.title}
+                  </h1>
 
-                    <div className="flex items-center gap-1">
-                      <Star />
-                      <p className="text-white">{movie.vote_average}</p>
-                      <p className="text-gray-400">/10</p>
-                    </div>
+                  <div className="flex items-center gap-1">
+                    <Star />
+                    <p className="text-white">{movie.vote_average}</p>
+                    <p className="text-gray-400">/10</p>
                   </div>
                 </div>
+              </div>
 
-                <div className="flex flex-col gap-5">
-                  <p className="text-white line-clamp-5">{movie.overview}</p>
+              <div className="flex flex-col gap-5">
+                <p className="text-white line-clamp-5">{movie.overview}</p>
 
-                  <div className="w-36 h-10 bg-white flex justify-center items-center rounded-md">
-                    <button className="flex items-center gap-2 text-black cursor-pointer">
-                      <Triangle />
-                      Watch Trailer
-                    </button>
-                  </div>
+                <div className="w-36 h-10 bg-white flex justify-center items-center rounded-md">
+                  <button className="flex items-center gap-2 text-black cursor-pointer">
+                    <Triangle />
+                    Watch Trailer
+                  </button>
                 </div>
               </div>
             </div>
 
-            {/* Right button */}
             <div className="pr-12">
               <div className="w-10 h-10 bg-white cursor-pointer flex justify-center items-center rounded-full">
                 <button className="cursor-pointer">
